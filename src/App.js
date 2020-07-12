47f38305d80b9a3779f6e48db347d34c898aed09
poomwarawat <58084725+poomwarawat@users.noreply.github.com>
2020-07-12 23:57:10 +0700

    Four assignment

--- src/App.js
+++ src/App.js
@@ -6,83 +6,60 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // input: "",
-      // todoArr: [],
-      r: 0,
-      g: 0,
-      b: 0,
+      name: "",
+      lastname: "",
+      phone: "",
+      address: "",
+      click: false,
     };
   }
-  // handleChange = (e) => {
-  //   const value = e.target.value;
-  //   this.setState({
-  //     input: value,
-  //   });
-  // };
-  // handleClick = () => {
-  //   this.setState({
-  //     todoArr: this.state.todoArr.concat(`${this.state.input}`),
-  //     input: "",
-  //   });
-  // };
-  // handleDelete = (e) => {
-  //   let index = e.currentTarget.id;
-  //   console.log(index);
-  //   this.setState({
-  //     todoArr: this.state.todoArr.splice(index, 1),
-  //   });
-  // };
-  handleRandom = () => {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
+  handleChange = (e) => {
+    const name = e.target.id;
+    const value = e.target.value;
     this.setState({
-      r: r,
-      g: g,
-      b: b,
+      [name]: value,
+    });
+  };
+  handleClick = () => {
+    this.setState({
+      click: true,
     });
   };
   render() {
-    const box = {
-      backgroundColor: `rgba(${this.state.r}, ${this.state.g}, ${this.state.b})`,
-      width: "300px",
-      height: "300px",
-      border: "5px solid black",
-    };
     return (
       <div className="p-4">
         <div className="container">
-          <div style={box}></div>
-          <button className="btn btn-primary mt-2" onClick={this.handleRandom}>
-            Random
-          </button>
-          {/* <h1>Todo list</h1>
-          {console.log(this.state.todoArr)}
-          <p>Comment your todo</p>
-          <input
-            className="form-control"
-            placeholder="input here"
-            onChange={this.handleChange}
-          />
+          <div>
+            <h1>name : </h1>
+            <input
+              id="name"
+              className="form-control"
+              onChange={this.handleChange}
+            />
+            <h1>lastname : </h1>
+            <input
+              id="lastname"
+              className="form-control"
+              onChange={this.handleChange}
+            />
+            <h1>phone : </h1>
+            <input
+              id="phone"
+              className="form-control"
+              onChange={this.handleChange}
+            />
+            <h1>address : </h1>
+            <input
+              id="address"
+              className="form-control"
+              onChange={this.handleChange}
+            />
+          </div>
           <button className="btn btn-primary mt-2" onClick={this.handleClick}>
-            send
+            Register
           </button>
-          {this.state.todoArr.length > 0 &&
-            this.state.todoArr.map((data, index) => {
-              return (
-                <div key={index} className="alert alert-primary mt-1">
-                  {data}{" "}
-                  <span
-                    id={index + 1}
-                    className="float-right"
-                    onClick={this.handleDelete}
-                    style={{ color: "red" }}
-                  >
-                    DELETE
-                  </span>
-                </div>
-              );
-            })} */}
+          <br />
+          {this.state.click === true && JSON.stringify(this.state)}
         </div>
       </div>
     );
